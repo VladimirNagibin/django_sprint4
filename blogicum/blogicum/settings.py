c7d3e14865bbab0ae6e463ad09853742c82e4b3d
Vladimir Nagibin <vlnagibin@yandex.ru>
2024-06-27 18:02:03 +0700

    Update settings

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -7,8 +7,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-9z7f9blb37oecy50sa!mh@77a$')
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-SECRET_KEY')
 
 DEBUG = True
 
